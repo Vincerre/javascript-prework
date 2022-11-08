96f05fa04f6d3600f4a747b91eb00c7d1866c8bb
Patryk Bazior <bazior.patryk@gmail.com>
2022-11-08 12:14:56 +0100

    Add js code

--- js/script.js
+++ js/script.js
@@ -1,14 +1,5 @@
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
-}
-
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
-
-let computerMove = 'kamień';
+let computerMove = `kamień`;
 let playerMove = 'papier';
-
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
+​
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to '+ playerMove +', to wygrywasz!');
+​
